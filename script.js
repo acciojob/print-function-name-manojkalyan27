1f5d0f2bab2876992ed83dd6b58467f50d0caba3
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2023-12-14 04:38:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function functionName() {
+	console.log(arguments.callee.name);
+}
+
+functionName();
+
+function AccioJob() {
+	console.log(arguments.callee.name);
+}
+
+AccioJob();
